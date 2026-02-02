538d7e20265fd5f7e6fa339f3db39b804409db99
Bimaahmadani <bimaahmadani@gmail.com>
2026-02-02 16:03:36 +0700

    add MyChat component

---
+++ app/components/MyChat.tsx
@@ -0,0 +1,13 @@
+import { useClient } from "@/hooks/useClient";
+import { User } from "@clerk/nextjs/server";
+import { 
+    Chat, 
+    Channel,
+    ChannelList,
+    ChannelHeader,
+    MessageList,
+    MessageInput,
+    Thread,
+    Window,
+ } from "stream-chat-react";
+ import 'stream-chat-react/dist/css/index.css';
