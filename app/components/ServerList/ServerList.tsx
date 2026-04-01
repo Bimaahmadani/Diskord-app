426fe1433af5bcd1f200c44f8f9ecbfedaf528bf
Bima Ahmadani <bimaahmadani@gmail.com>
2026-04-01 14:13:43 +0700

    update ServerList

--- app/components/ServerList/ServerList.tsx
+++ app/components/ServerList/ServerList.tsx
@@ -1,5 +1,6 @@
 import { DiscordServer } from "@/models/DiscordServer";
 import {v4 as uuid} from 'uuid';
+import Image from "next/image";
 
 export default function ServerList() {
     const servers: DiscordServer[] = [
@@ -22,9 +23,31 @@ export default function ServerList() {
     ];
     return <div className="bg-blue-500">
         {servers.map(server => (
-            <button key={server.id} onClick={() => console.log(`Clicked on server ${server.name}`)}>
-                {server.name}
+            <button 
+            key={server.id} 
+            onClick={() => console.log(`Clicked on server ${server.name}`)}>
+             {server.image && checkIfurl(server.image) ? (
+                <Image
+                src={server.image}
+                width={50}
+                height={50}
+                alt='Server Icon'
+                />
+             ) :(
+                <span className="bg-gray-600 w-[50px] flex items-center justify-center text-sm rounded-full">
+                    {server.name.charAt(0)}
+                </span>
+             )}
             </button>
             ))}
     </div>
+}
+
+function checkIfurl(path: string): Boolean {
+    try {
+        const _ = new URL(path);
+        return true;
+    } catch (_) {
+        return false;
+    }
 }
