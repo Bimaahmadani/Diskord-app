09fe62c4d4a0fec07071aeac74b26eaca808d66a
Bima Ahmadani <bimaahmadani@gmail.com>
2026-04-10 20:38:36 +0700

    update ServerList

--- app/components/ServerList/ServerList.tsx
+++ app/components/ServerList/ServerList.tsx
@@ -1,37 +1,42 @@
 import { DiscordServer } from "@/models/DiscordServer";
 import {v4 as uuid} from 'uuid';
 import Image from "next/image";
+import { useState } from "react";
+
 
 export default function ServerList() {
+    const [activeServer, setActiveServer] = useState<DiscordServer| undefined>();
     const servers: DiscordServer[] = [
         {
-            id: uuid(),
+            id: '1',
             name: 'Test Server 1',
-            image: ''
+            image: 'https://images.unsplash.com/photo-1531297484001-80022131f5a1?q=80&w=1120&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
         },
         {
-            id: uuid(),
+            id: '2',
             name: 'Test Server 2',
-            image: ''
+            image: 'https://images.unsplash.com/photo-1611162617213-7d7a39e9b1d7?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OTN8fHRlY2hub2xvZ3l8ZW58MHx8MHx8fDA%3D'
         },
         {
-            id: uuid(),
+            id: '3',
             name: 'Test Server 3',
-            image: ''
+            image: 'https://images.unsplash.com/photo-1485827404703-89b55fcc595e?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjB8fHRlY2hub2xvZ3l8ZW58MHx8MHx8fDA%3D'
         },
 
     ];
-    return <div className="bg-dark-gray h-full flex flex-col items-center gap-2">
+    return <div className="bg-dark-gray h-full flex flex-col items-center gap-y-3">
         {servers.map(server => (
             <button 
-            key={server.id} 
-            onClick={() => console.log(`Clicked on server ${server.name}`)}>
+            key={server.id}
+            className={`sidebar-icon ${server.id === activeServer?.id ? 'selected-icon' : ''}`} 
+            onClick={() => setActiveServer(server)}>
              {server.image && checkIfurl(server.image) ? (
                 <Image
                 src={server.image}
                 width={50}
                 height={50}
                 alt='Server Icon'
+                className="rounded-icon"
                 />
              ) :(
                 <span className="rounded-icon bg-gray-600 w-[50px] flex items-center justify-center text-sm">
