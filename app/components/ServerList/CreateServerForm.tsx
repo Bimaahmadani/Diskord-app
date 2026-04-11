24d4839aed5c4a2d1fd2c6e11147c6b1e4ec2dc4
Bima Ahmadani <bimaahmadani@gmail.com>
2026-04-11 17:18:06 +0700

    update CreateServerForm

--- app/components/ServerList/CreateServerForm.tsx
+++ app/components/ServerList/CreateServerForm.tsx
@@ -1,4 +1,15 @@
+import Link from "next/link"
+
 export default function CreateServerForm() {
-    return <div>Creating a server...</div>
+    return (
+    <dialog className="absolute z-10 space-y-2 rounded-xl">
+        <div className="w-full flex items-center justify py-8 px-6">
+            <h2 className="text-3xl font-semibold text-gray-600">
+                Create a Server
+            </h2>
+            <Link href='/'></Link>
+        </div>
+    </dialog>
+)
 }
     
