75ae6765c17cc7df3501037a2bcc779a2961a4af
Bima Ahmadani <bimaahmadani@gmail.com>
2026-04-15 17:20:45 +0700

    add UserRow

--- app/components/ServerList/CreateServerForm.tsx
+++ app/components/ServerList/CreateServerForm.tsx
@@ -1,8 +1,8 @@
 import { useSearchParams } from "next/dist/client/components/navigation";
 import Link from "next/link"
-import { use, useEffect, useRef, useState } from "react"
+import { use, useCallback, useEffect, useRef, useState } from "react"
 import { JSX } from "react/jsx-dev-runtime"
-import { CloseIcon } from "stream-chat-react"
+import { CloseIcon, useChat, useChatContext } from "stream-chat-react"
 import { UserObject } from "@/models/UserObject";
 
 interface Props {
@@ -19,10 +19,13 @@ type FormState = {
 
 
 export default function CreateServerForm({ open, setOpen }: Props): JSX.Element {
+    //check if we are shown
     const params = useSearchParams();
     const showCreateServerForm = params.get('createServer');
     const dialogRef = useRef<HTMLDialogElement>(null);
 
+    //Data
+    const {client}= useChatContext();
     const initialState: FormState = {
         serverName: '',
         serverImage: '',
@@ -30,12 +33,33 @@ export default function CreateServerForm({ open, setOpen }: Props): JSX.Element 
     };
 
     const [formData, setFormData] = useState<FormState>(initialState);
+    const [user, setUser] = useState<UserObject []>([]);
+
+    const loadUsers = useCallback(async () => {
+        const response = await client.queryUsers({});
+        const users: UserObject[] = response.users
+            .filter((user) => user.role !== "admin")
+            .map((user) => {
+                return {
+                    id: user.id,
+                    name: user.name ?? user.id,
+                    image: user.image as string,
+                    online: user.online,
+                    lastOnline: user.last_active
+                }
+            });
+            if (users) setUser(users);
+    }, [client]);
 
     useEffect(() => {
         if (showCreateServerForm==='true') {
             setOpen(true);
         }
     }, [showCreateServerForm]);
+
+    useEffect(() => {
+        loadUsers();
+    }, [loadUsers]);
 
     useEffect(() => {
     const dialog = dialogRef.current;
@@ -78,10 +102,12 @@ export default function CreateServerForm({ open, setOpen }: Props): JSX.Element 
         </div>
         
         <form method="dialog" className="flex flex-col space-y-2 px-6" action="">
+            
+            {/* Server Name Form */}
             <label className="labelTitle" htmlFor="serverName">
                 Server Name
             </label>
-            <div className="flex items-center bg-gray-100">
+            <div className="flex items-center bg-gray-100 rounded">
                 <span className="text-2xl p-2 text-gray-500">#</span>
                 <input
                     type="text"
@@ -92,7 +118,29 @@ export default function CreateServerForm({ open, setOpen }: Props): JSX.Element 
                         setFormData({ ...formData, serverName: e.target.value })}
                     required
                 />
+            </div>
 
+            {/* Image form */}
+            <label className="labelTitle" htmlFor="serverImage">
+                Image URL
+            </label>
+            <div className="flex items-center bg-gray-100 rounded">
+                <span className="text-2xl p-2 text-gray-500">#</span>
+                <input
+                    type="text"
+                    id="serverImage"
+                    name="serverImage"
+                    value={formData.serverImage}
+                    onChange={(e) => 
+                        setFormData({ ...formData, serverImage: e.target.value })}
+                    required
+                />
+            </div>
+
+            <div className="max-h-64 overflow-y-scroll">
+                {user.map((user) => (
+                    
+                ))}
             </div>
         </form>
     </dialog>
---
+++ app/components/UserRow.tsx
@@ -0,0 +1,38 @@
+import { UserObject } from "@/models/UserObject";
+import Image from "next/image";
+import { JSX } from "react/jsx-dev-runtime";
+import { PersonIcon } from "./Icons";
+
+export default function UserRow({
+    user,
+    userChanged
+}: {
+    user: UserObject
+    userChanged: (user: UserObject, checked: boolean) => void
+}): JSX.Element {
+    return(
+        <div className="flex items-center justify-start w-full space-x-4 my-2">
+            <input
+            id={user.id} 
+            type="checkbox"
+            name={user.id}
+            className="w-4 h-4 mb-0"
+            onChange={(event) =>{
+                userChanged(user, event.target.checked)
+            }} 
+            ></input>
+            <label className="w-full flex itmes-center space-x-6" htmlFor="users">
+                {user.image &&(
+                  <Image
+                    src={user.image}
+                    width={40}
+                    height={40}
+                    alt={user.name}
+                    className="w-8 h-8 rounded-full"
+                  />
+                )}
+                {!user.image && <PersonIcon />}
+            </label>
+        </div>
+    )
+}
