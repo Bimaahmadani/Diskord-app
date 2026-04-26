16d59d92ddce8ac4eabefefc18429e57798bcc83
Bima Ahmadani <bimaahmadani@gmail.com>
2026-04-26 22:44:11 +0700

    update DiscordContext

--- app/contexts/DiscordContext.tsx
+++ app/contexts/DiscordContext.tsx
@@ -1,12 +1,14 @@
 'use client';
 import { DiscordServer } from "@/models/DiscordServer";
+import { channel } from "diagnostics_channel";
 import { createContext, use, useCallback, useContext, useState } from "react";
-import { StreamChat } from "stream-chat";
+import { Channel, StreamChat, ChannelFilters } from "stream-chat";
 import { DefaultChannelData } from "stream-chat-react";
 import { v4 as uuid } from "uuid";
 
 type DiscordState= {
     server?: DiscordServer;
+    channelsByCategories: Map<string, Array<Channel<DefaultStreamChatGenerics>>>
     changeServer:(server: DiscordServer | undefined, client: StreamChat) => void;
     createServer:(
         client: StreamChat,
@@ -18,6 +20,7 @@ type DiscordState= {
 
 const initialValue: DiscordState = {
     server: undefined,
+    channelsByCategories: new Map(),
     createServer: () => {},
     changeServer: () => {},
 };
@@ -45,11 +48,51 @@ export const DiscordContextProvider: any = ({
 
     const changeServer = useCallback(
         async (server: DiscordServer | undefined, client: StreamChat) => {
+            let filters: ChannelFilters ={
+                type: 'messaging',
+                members: {$in: [client.userID as string]},
+            };
+            if (!server){
+                filters.member_count = 2;
+            }
+
+            const channels = await client.queryChannels(filters);
+            const channelsByCategories = new Map<
+                string,
+                Array<Channel<DefaultStreamChatGenerics>>
+            >();
+
+            if(server){
+                const categories = new Set(
+                    channels
+                    .filter((channel)=>{
+                        return channel.data?.data?.server === server.name
+                    })
+                    .map((channel)=>{
+                        return channel.data?.data?.category;
+                    })
+                );
+
+                for (const category of Array.from(categories)){
+                    channelsByCategories.set(
+                        category,
+                        channels.filter((channel)=>{
+                            return(
+                                channel.data?.data?.server === server.name &&
+                                channel.data?.data?.category === category
+                            );
+                        })
+                    )
+                }
+            } else{
+                channelsByCategories.set('Find or start a conversation', channels);
+            }
             setMyState((myState)=>{
                 return {
                     ...myState,
                     server: server,
-                }
+                    channelsByCategories
+                };
             });
         },
         [setMyState]
@@ -86,6 +129,7 @@ export const DiscordContextProvider: any = ({
 
     const store: DiscordState= {
         server: myState.server,
+        channelsByCategories: myState.channelsByCategories,
         changeServer: changeServer,
         createServer: createServer,
     };
