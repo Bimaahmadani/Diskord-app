b416acf1d915a8c60a9fa616475cee6910428a4b
Bima Ahmadani <bimaahmadani@gmail.com>
2026-05-03 22:22:57 +0700

    update CustomMessage

--- app/components/MessageList/CustomMessage/CustomMessage.tsx
+++ app/components/MessageList/CustomMessage/CustomMessage.tsx
@@ -1,12 +1,18 @@
-import { JSX } from "react";
-import { useMessageContext } from "stream-chat-react";
+import { JSX, useState } from "react";
+import { ReactionsList, useMessageContext, ReactionSelector } from "stream-chat-react";
 import Image from "next/image";
+import MessageOptions from "./MessageOptions";
 
 export default function CustomMessage(): JSX.Element {
     const {message} = useMessageContext();
+    const [showOptions, setShowOptions] = useState(false);
+        const [showReactions, setShowReactions] = useState(false);
 
     return (
-        <div className="flex relative space-x-2 p-2 rounded-md transition-colors ease-in-out duration-200 hover:bg-gray-100">
+        <div 
+        onMouseEnter={()=>setShowOptions(true)}
+        onMouseLeave={()=>setShowOptions(false)}
+        className="flex relative space-x-2 p-2 rounded-md transition-colors ease-in-out duration-200 hover:bg-gray-100">
             <Image
             className="rounded-full aspect-square object-cover w-10 h-10"
             width={40}
@@ -16,6 +22,14 @@ export default function CustomMessage(): JSX.Element {
             />
 
             <div>
+                {showOptions &&(
+                    <MessageOptions showEmojiReactions={setShowReactions}/>
+                )}
+                {showReactions &&(
+                   <div className="absolute">
+                        <ReactionSelector/>
+                   </div>
+                )}
                 <div className="space-x-2">
                     <span className="'font-semibold text-sm text-black">
                         {message.user?.name}
@@ -28,6 +42,7 @@ export default function CustomMessage(): JSX.Element {
                 </div>
                 <p className="text-sm text-gray-700">
                     {message.text}
+                    <ReactionsList/>
                 </p>
             </div>
         </div>
