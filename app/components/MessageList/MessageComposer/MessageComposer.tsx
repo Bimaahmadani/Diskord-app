e4a9bcbe9ead3e95f9f6c03c06681256490a203d
Bima Ahmadani <bimaahmadani@gmail.com>
2026-05-07 22:42:39 +0700

    update MessageComposer

--- app/components/MessageList/MessageComposer/MessageComposer.tsx
+++ app/components/MessageList/MessageComposer/MessageComposer.tsx
@@ -1,6 +1,6 @@
 import { JSX, useState } from "react";
 import { SendButton, useChatContext } from "stream-chat-react";
-import { GIF, PlusCircle, Present } from "../../Icons";
+import { GIF, PlusCircle, Present, Send } from "../../Icons";
 import { plusItems } from "./plusItem";
 import ChannelListMenuRow from "../../ChannelList/TopBar/ChannelListMenuRow";
 import CustomEmojiPicker from "./CustomEmojiPicker";
@@ -13,6 +13,14 @@ export default function MessageComposer(): JSX.Element {
     // Fungsi penangkap emoji dari CustomEmojiPicker
     const handleEmojiSelect = (emojiChar: string) => {
         setMessage((prevMessage) => prevMessage + emojiChar);
+    };
+
+    const handleSend = (event: React.BaseSyntheticEvent) => {
+        event.preventDefault(); // Mencegah form refresh bawaan
+        if (message.trim() !== '') {
+            channel?.sendMessage({ text: message });
+            setMessage('');
+        }
     };
 
     return(
@@ -39,18 +47,29 @@ export default function MessageComposer(): JSX.Element {
             type="text"
             value={message}
             onChange={(e) => setMessage(e.target.value)}
+            onKeyDown={(e) => {
+                    if (e.key === 'Enter' && message.trim() !== '') {
+                        channel?.sendMessage({ text: message });
+                        setMessage('');
+                    }
+                }}
             placeholder={`Message #${channel?.data?.name || 'general'}`}
             className="border-transparent bg-transparent outline-none text-sm font-semibold m-0 text-gray-normal" 
             />
             <Present className="w-8 h-8 hover:text-gray-800"/>
             <GIF className="w-8 h-8 hover:text-gray-800"/>
             <CustomEmojiPicker onEmojiSelect={handleEmojiSelect} />
-            <SendButton
-                sendMessage={()=>{
-                    channel?.sendMessage({text: message});
-                    setMessage('');
-                }}
-            />
+           <button
+                onClick={handleSend}
+                disabled={message.trim() === ''}
+                className={`font-bold uppercase text-sm p-2 transition-all duration-200 ${
+                    message.trim() === '' 
+                    ? 'text-gray-400 cursor-not-allowed opacity-50' 
+                    : 'text-gray-600 hover:text-gray-800 cursor-pointer'
+                }`}
+            >
+                <Send />
+            </button>
         </div>
     )
 
