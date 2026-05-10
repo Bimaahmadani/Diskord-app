dbdef4ae1cd32a2df01e7e3adf5bfd68cfedeeca
Bima Ahmadani <bimaahmadani@gmail.com>
2026-05-10 22:34:35 +0700

    update MyCall

---
+++ app/components/MyCall/MyCall.tsx
@@ -0,0 +1,45 @@
+import { JSX, useState, useCallback, useEffect } from "react";
+import { Call, StreamCall, useStreamVideoClient } from "@stream-io/video-react-sdk";
+
+export default function MyCall({callId}: {callId: string}) : JSX.Element{
+    const [call, setCall] = useState<Call | undefined>(undefined)
+    const client = useStreamVideoClient();
+
+    const [joining, setJoining] = useState(false);
+
+    const createCall = useCallback(async ()=>{
+        const callToCreate = client?.call('default', callId);
+        await callToCreate?.camera.disable();
+        await callToCreate?.join({create:true});
+        setCall(callToCreate);
+        setJoining(false);
+    }, [client, callId]);
+
+    useEffect(()=>{
+        if(!callId){
+            console.error('No client in MyCall component');
+            return;
+        }
+
+        if(!call){
+            if(joining){
+                createCall()
+            } else{
+                setJoining(true)
+            }
+        }
+    }, [call, client, createCall, joining])
+
+    if(!call){
+        return(
+            <div className="w-full h-full text-xl font-semibold flex items-center justify-center animate-pulse">
+                Joining call ...
+            </div>
+        );
+    }
+    
+    return(
+        <StreamCall call={call}>
+        </StreamCall>
+    )
+}
