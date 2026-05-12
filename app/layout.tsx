0b891ad268bcd5410d573bc4a4c87be58952fbdd
Bima Ahmadani <bimaahmadani@gmail.com>
2026-05-12 22:18:51 +0700

    update layout, deleting the header

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,7 +36,7 @@ export default function RootLayout({
       <html lang="en">
         <DiscordContextProvider>
         <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-          <header className="flex justify-end items-center p-4 gap-4 h-16">
+          {/* <header className="flex justify-end items-center p-4 gap-4 h-16">
             <SignedOut>
               <SignInButton />
               <SignUpButton>
@@ -48,7 +48,7 @@ export default function RootLayout({
             <SignedIn>
               <UserButton />
             </SignedIn>
-          </header>
+          </header> */}
           {children}
         </body>
         </DiscordContextProvider>
